ed376edc145bdeeca046839d33ff2c0503d5e5e1
nhatphamtienICT <nhatzzpham@gmail.com>
2024-09-13 23:15:20 +0700

    Add tablayout, remove previous icons

--- app/src/main/java/vn/edu/usth/facebookclient/HomeFragmentPagerAdapter.java
+++ app/src/main/java/vn/edu/usth/facebookclient/HomeFragmentPagerAdapter.java
@@ -6,8 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+
 public class HomeFragmentPagerAdapter extends FragmentStateAdapter {
-    private final String[] titles = new String[] { "Hanoi", "Paris", "Toulouse" };
     public HomeFragmentPagerAdapter(AppCompatActivity activity) {
         super(activity);
     }
@@ -16,16 +16,24 @@ public class HomeFragmentPagerAdapter extends FragmentStateAdapter {
 
     @Override
     public int getItemCount() {
-        return 3; // number of pages for a ViewPager
+
+        return 5; // number of pages for a ViewPager
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return new MakePostFragment(); // failsafe
+        switch (position){
+            case 0: return new PostList();
+            case 1:
+            case 2:
+            case 3:
+                return new MakePostFragment();
+        }
+        return new Notifications();
     }
-    public CharSequence getPageTitle(int page) {
-// returns a tab title corresponding to the specified page
-        return titles[page];
-    }
+//    public CharSequence getPageTitle(int page) {
+//// returns a tab title corresponding to the specified page
+//        return titles[page];
+//    }
 }
