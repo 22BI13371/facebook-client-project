9c7a3a42eae13ac06e32347e3e78477f00730c6f
Quân Nguyễn <155754824+22BI13371@users.noreply.github.com>
2024-10-14 19:38:24 +0700

    Added friends view switching functionality

--- app/src/main/java/vn/edu/usth/facebookclient/friend_list.java
+++ app/src/main/java/vn/edu/usth/facebookclient/friend_list.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +21,8 @@ public class friend_list extends Fragment {
 
     private RecyclerView friendRequestsRecyclerView;
     private RecyclerView peopleYouMayKnowRecyclerView;
+    private RecyclerView friendListRecyclerView;
+    private TextView textView;
 
     public friend_list() {
         // Required empty public constructor
@@ -38,14 +42,40 @@ public class friend_list extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friend_list, container, false);
+        textView = view.findViewById(R.id.friendListTextView);
 
         // Initialize RecyclerViews
         friendRequestsRecyclerView = view.findViewById(R.id.friendRequestsRecyclerView);
 //        peopleYouMayKnowRecyclerView = view.findViewById(R.id.peopleYouMayKnowRecyclerView);
+        friendListRecyclerView = view.findViewById(R.id.friendListRecyclerView);
 
         // Set layout managers
         friendRequestsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 //        peopleYouMayKnowRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        friendListRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        friendListRecyclerView.setVisibility(View.INVISIBLE);
+
+        Button myFriendButton = view.findViewById(R.id.myFriendsButton);
+        Button friendRequestButton = view.findViewById(R.id.friendRequestButton);
+
+        myFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                friendRequestsRecyclerView.setVisibility(View.INVISIBLE);
+                friendListRecyclerView.setVisibility(View.VISIBLE);
+                textView.setText(R.string.my_friends);
+            }
+        });
+
+        friendRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                friendListRecyclerView.setVisibility(View.INVISIBLE);
+                friendRequestsRecyclerView.setVisibility(View.VISIBLE);
+                textView.setText(R.string.friend_requests);
+            }
+        });
 
         // Sample data for friend requests and people you may know
         List<String> friendRequests = Arrays.asList("John", "Sara", "Michael", "Anna", "David", "John", "Sara", "Michael", "Anna", "David", "John", "Sara", "Michael", "Anna", "David");
